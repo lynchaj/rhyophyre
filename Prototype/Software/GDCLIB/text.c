4ff429fe4ddb0270ced129e24d48df49bd4a65eb
Wayne Warthen <wwarthen@gmail.com>
2022-04-28 20:19:07 -0700

    TEXT Application Cleanup
    - Move font data to separate file
    - Parameter driven execution
    - Can now "type" a file to display
    - Optional inter-character delay
    - Add key to perform reverse linefeed
    - Add key to perform color change
    - Implement backspace

--- Prototype/Software/GDCLIB/text.c
+++ Prototype/Software/GDCLIB/text.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "gdc7220.h"
+
+const char * usage =
+    "Usage: TEXT <options>\n"
+    "    -i            - interactively echo keys typed to display\n"
+    "    -f <filename> - type file to display\n"
+    "    -t            - show character test pattern and exit\n"
+    "    -d            - add delay between char writes\n"
+    "    -h            - display help\n"
+    "\n"
+    "TEXT sends text output to the uPD7220 display.\n"
+    "It will display a test pattern (-t), echo typed\n"
+    "keyboard input (-i), or display file contents (-f).\n"
+    "The following special keys are accepted:\n"
+    "    ctrl-A         - reverse linefeed\n"
+    "    ctrl-B         - switch to next color\n";
+
 
 void delay(uint16_t n)
 {
@@ -28,24 +45,52 @@ int main(int argc, char *argv[])
 {
     int ch = 0, have_error = 0;
     int x, y;
+    uint8_t cur_color;
+    char filename[80] = "";
+    int interactive = 0;
+    int testpat = 0;
+    int usedelay = 0;
+    int showusage = 1;
 
-    const char *optstring = "C";
+    const char *optstring = "F:TDHI";
 
     while ((ch = getopt(argc, argv, optstring)) != -1)
     {
         switch (ch)
         {
-            case 'C':
-            case 'c':
+            case 'F':
+                strcpy(filename, optarg);
+                interactive = 0;
+                showusage = 0;
                 break;
-//            case '?':
-//                break;
-            default:
+            
+            case 'I':
+                interactive = 1;
+                showusage = 0;
+                break;
+
+            case 'T':
+                testpat = 1;
+                showusage = 0;
+                break;
+            
+            case 'D':
+                usedelay = 1;
+                break;
+                
+            case 'H':
+                showusage = 1;
                 break;
         }
     }
 
-    printf("Start upd7220 TYPE test.\n");
+    printf("uPD7220 Text Display Tool (use -H for help)\n\n");
+
+    if (showusage)
+    {
+        printf(usage);
+        exit(0);
+    }
 
     if (!init_gdc_system(MODE_640X480)) {
         printf("Failed to initialize UPD7220 video.\n");
@@ -62,60 +107,102 @@ int main(int argc, char *argv[])
     color_line(0, Ymax-1, Xmax-1, Ymax-1);
 
     uint8_t c = 0;
-
-    for (int y = 0; y < 16; y++)
+    
+    if (testpat)
     {
-        color_setup((y + CLR_WHITE) % 16);
-        for (int x = 0; x < 16; x++)
+        printf("Displaying character test pattern...\n");
+        for (int y = 0; y < 16; y++)
         {
-            // delay(10000);
-            text_set_cursor(y, x);
-            text_write_char(c++);
+            color_setup((y + CLR_WHITE) % 16);
+            for (int x = 0; x < 16; x++)
+            {
+                // delay(10000);
+                text_set_cursor(y, x);
+                text_write_char(c++);
+            }
         }
+        
+        exit(0);
     }
-    waitkey();
     
-    printf("Type stuff on CP/M console (<esc> to end)...\n");
-    
+    FILE *fp;
+    if (interactive)
+    {
+        printf("Type stuff on CP/M console (ctrl-Z to end)...\n");
+        fp = stdin;
+    }
+    else
+    {
+        if (!(fp = fopen(filename, "r")))
+        {
+            printf("ERROR: File %s could not be opened!\n", filename);
+            exit(1);
+        }
+        printf("Typing file %s to display...\n", filename);
+    }
+
     x = y = 0;
     text_set_cursor(y, x);
     text_clear_screen();
     text_show_cursor();
-    color_setup(CLR_WHITE);
-
-    while (1)
+    cur_color = CLR_WHITE;
+    color_setup(cur_color);
+    
+    while ((ch = getc(fp)) != EOF)
     {
-        c = getchar();
-        
-        if (c == 27)
-            break;
+        if (usedelay)
+            delay(2500);
 
-        if (c == '\n')
+        switch(ch)
+        {
+            case 1:     // ctrl A - reverse linefeed
+                y--;
+                break;
+            
+            case 2:     // ctrl B - next color
+                cur_color = ++cur_color % 16;
+                color_setup(cur_color);
+                break;
+                
+            case 8:     // backspace
+                x--;
+                break;
+                
+            case '\n':
+                x = 0;
+                y++;
+                break;
+            
+            default:
+                text_write_char(ch);
+                x++;
+                break;
+        }
+        
+        if (x < 0)
+            x = 0;
+        if (x >= 80)
         {
             x = 0;
             y++;
         }
-        else
+        if (y < 0)
         {
-            text_write_char(c);
-            x++;
-            if (x >= 80)
-                x = 79;
+            text_clear_lines(-1, 1);
+            text_scroll(1);
+            y = 0;
         }
-        
         if (y >= 60)
         {
             text_clear_lines(60, 1);
             text_scroll(-1);
             y = 59;
         }
-
+        
         text_set_cursor(y, x);
     }
 
-    gdc_done();
-
-    printf("\nEnd test of uPD7220\n");
+    fclose(fp);
 
     return 0;
 }
